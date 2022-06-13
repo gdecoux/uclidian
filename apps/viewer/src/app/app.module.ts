e876f68b58507fc6db42fa6f5d85af9f241f0312
Grant <gadecoux@gmail.com>
2022-06-13 16:47:31 -0500

    feat(viewer): viewer app boilerplate

---
+++ apps/viewer/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  imports: [BrowserModule, BrowserAnimationsModule, HttpClientModule, AppRoutingModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
